f72d33a6a42c1a778bd00ced0602ee68b7dcda91
John Meehan <john.f.meehan@gmail.com>
2013-04-14 22:55:24 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woodshop::Application.config.secret_token = 'ac9cf6b7b2616463dda6c3b5f719887af9ccfe50286f670035813d874661d64c8e9a275f48c0b84942959e9ef750d76f8c7c3aaead12074a181eb0ba5002cbf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Woodshop::Application.config.session_store :cookie_store, key: '_woodshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Woodshop::Application.config.session_store :active_record_store
